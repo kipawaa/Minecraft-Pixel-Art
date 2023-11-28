4f28902213166a31998bc371a5614be569a6e7e5
River McCubbin <mccubbinri@gmail.com>
2023-11-28 09:35:09 -0500

    now outputs minecraft image

--- converter.py
+++ converter.py
@@ -32,10 +32,31 @@ def get_closest_colour(colour, colour_data):
             closest_dist = dist
     return closest
 
+
+def get_closest_file(colour, colour_data):
+    # return the closest colour in get_block_colours to colour
+
+    # cast input to numpy array
+    target_colour = np.array(colour)
+
+    closest = np.zeros(4)
+    closest_dist = 10000
+    closest_file = ""
+    for colour, filename in colour_data:
+        dist = np.linalg.norm(colour - target_colour) 
+        if dist < closest_dist:
+            closest = colour
+            closest_dist = dist
+            closest_file = filename
+    return closest_file
+
+
 if __name__ == "__main__":
     # get image file
     user=input("input the name of your art file, with the extension: ")
     with Image.open(user.strip()) as image:
+        if image.mode != "RGBA":
+            image = image.convert('RGBA')
 
         # check for resizing
         user=input("would you like to resize? (y/n): ")
@@ -61,6 +82,7 @@ if __name__ == "__main__":
 
             # create a new image
             approximation = np.zeros((image.width, image.height, 4))
+            mc_art = Image.new('RGBA', (image.width*16, image.height*16))
 
             # get pixels from input image
             pixels=image.load()
@@ -71,8 +93,10 @@ if __name__ == "__main__":
                 for x in range(image.width):
                     # get the best approximation of this pixel
                     approximation[y,x, :] = get_closest_colour(pixels[x,y], colour_data)
+                    with Image.open("./blocks/" + get_closest_file(pixels[x,y], colour_data)) as block_image:
+                        mc_art.paste(block_image, box=(16*y, 16*x))
             print("generated approximation")
 
             # show the resulting image
-            mc_art = Image.fromarray(approximation.astype('uint8'))
-            mc_art.show()
+            approximation_image = Image.fromarray(approximation.astype('uint8'))
+            mc_art.rotate(90).show()
